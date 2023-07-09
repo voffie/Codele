07ff41e412946a729e3e802bd8c59beff65dd093
VoffieDev <viktorsoderborg@gmail.com>
2023-07-09 17:04:22 +0200

    Add game logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,147 @@
 import Div100vh from "react-div-100vh";
 import { Navbar } from "./components/Header";
 import { FormProvider, useForm } from "react-hook-form";
+import { useState } from "react";
+import { ILanguage, getGuessData } from "./data";
 
 const App = () => {
+  const totalGuesses = 5;
+  const [isGameWon, setGameWon] = useState(false);
+  const [isGameOver, setGameOver] = useState(false);
+  const [guesses, setGuesses] = useState<ILanguage[]>([]);
   const methods = useForm();
-  const { handleSubmit, register } = methods;
+  const { handleSubmit, register, reset } = methods;
+  const solution = {
+    name: "Ruby",
+    creator: "Yukihiro Matsumoto",
+    releaseYear: 1995,
+    compiled: false,
+    objectOriented: true,
+  };
 
   const onSubmit = handleSubmit((values) => {
-    console.log(values);
+    const info = getGuessData(values.currentGuess);
+    if (!info) {
+      reset();
+      return null;
+    }
+    setGuesses((old) => [...old, info]);
+
+    reset();
+
+    if (info.name === solution.name) {
+      setGameWon(true);
+    } else if (guesses.length === 4 && info.name !== solution.name) {
+      setGameOver(true);
+    }
   });
+
   return (
     <Div100vh>
-      <div className="flex h-full flex-col bg-black">
+      <div className="flex h-full flex-col bg-black overflow-hidden">
         <Navbar />
-        <div className="flex flex-col gap-20 h-full w-full text-white terminal">
+        <div className="flex flex-col gap-5 h-full w-full text-white terminal">
+          <div className="m-2">
+            <h1>Welcome to Codele!</h1>
+            <h1>
+              The aim of the game is to guess the right programming language
+            </h1>
+            <p>FYI: Not all languages are added at this moment</p>
+            <h1>You have {totalGuesses - guesses.length} guesses remaining</h1>
+          </div>
           <FormProvider {...methods}>
             <form
               onSubmit={onSubmit}
               className="m-2"
             >
               <div className="flex gap-2">
-                <h1>guest@codele.dev</h1>
+                <h1>$ guest@codele.dev</h1>
                 <input
                   autoFocus
                   type="text"
+                  disabled={isGameOver || isGameWon}
                   className="appearance-none bg-transparent focus:outline-none selection:bg-white selection:text-black"
                   {...register("currentGuess")}
                 />
               </div>
             </form>
           </FormProvider>
+          <table className="border border-white-400 text-center">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Creator(s)</th>
+                <th>Release Year</th>
+                <th>Interpreted</th>
+                <th>Object Oriented</th>
+              </tr>
+            </thead>
+            <tbody>
+              {guesses.map((guess, index) => (
+                <tr key={index}>
+                  <td
+                    className={
+                      guess.name === solution.name
+                        ? "text-green-300"
+                        : "text-red-300"
+                    }
+                  >
+                    {guess.name}
+                  </td>
+                  <td className="p-2">
+                    {typeof guess.creator === "object"
+                      ? guess.creator.map((creator) => <p>{creator}</p>)
+                      : guess.creator}
+                  </td>
+                  <td
+                    className={
+                      guess.releaseYear === solution.releaseYear
+                        ? "text-green-300"
+                        : guess.releaseYear === "No data"
+                        ? "text-yellow-300"
+                        : "text-red-300"
+                    }
+                  >
+                    {guess.releaseYear}
+                  </td>
+                  <td
+                    className={
+                      guess.compiled === solution.compiled
+                        ? "text-green-300"
+                        : "text-red-300"
+                    }
+                  >
+                    {guess.compiled ? "True" : "False"}
+                  </td>
+                  <td
+                    className={
+                      guess.objectOriented === solution.objectOriented
+                        ? "text-green-300"
+                        : "text-red-300"
+                    }
+                  >
+                    {guess.objectOriented ? "True" : "False"}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          {isGameWon && (
+            <div className="text-center">
+              <h1>YOU WON!</h1>
+              <h1>The correct answer was {solution.name}!</h1>
+              <h1>
+                You solved it with {totalGuesses - guesses.length} guesses
+                remaining
+              </h1>
+            </div>
+          )}
+          {isGameOver && (
+            <div className="text-center">
+              <h1>YOU LOST!</h1>
+              <h1>The correct answer was {solution.name}!</h1>
+            </div>
+          )}
         </div>
       </div>
     </Div100vh>
---
+++ src/data.tsx
@@ -0,0 +1,121 @@
+export interface ILanguage {
+  name: string;
+  creator: string | string[];
+  releaseYear: number | string;
+  compiled: boolean;
+  objectOriented: boolean;
+}
+
+export const getGuessData = (guess: string) => {
+  return languages.find(
+    (item) => item.name.toLowerCase() === guess.toLowerCase()
+  );
+};
+
+const languages = [
+  {
+    name: "ActionScript",
+    creator: "Gary Grossman",
+    releaseYear: 1998,
+    compiled: true,
+    objectOriented: true,
+  },
+  {
+    name: "AngleScript",
+    creator: "Andreas Jönsson",
+    releaseYear: 2003,
+    compiled: true,
+    objectOriented: true,
+  },
+  {
+    name: "Bash",
+    creator: "Brian Fox",
+    releaseYear: 1989,
+    compiled: false,
+    objectOriented: false,
+  },
+  {
+    name: "BASIC",
+    creator: ["John G. Kemeny", "Thomas E. Kurtz"],
+    releaseYear: 1964,
+    compiled: false,
+    objectOriented: true,
+  },
+  {
+    name: "C++",
+    creator: "Bjarne Stroustrup",
+    releaseYear: 1985,
+    compiled: true,
+    objectOriented: true,
+  },
+  {
+    name: "C#",
+    creator: "Microsoft",
+    releaseYear: 2000,
+    compiled: true,
+    objectOriented: true,
+  },
+  {
+    name: "Carbon",
+    creator: "Google",
+    releaseYear: "No data",
+    compiled: true,
+    objectOriented: true,
+  },
+  {
+    name: "Clojure",
+    creator: "Rich Hickey",
+    releaseYear: 2007,
+    compiled: true,
+    objectOriented: false,
+  },
+  {
+    name: "Dart",
+    creator: ["Lars Bak", "Kasper Lund"],
+    releaseYear: 2011,
+    compiled: true,
+    objectOriented: true,
+  },
+  {
+    name: "Elixir",
+    creator: "José Valim",
+    releaseYear: 2012,
+    compiled: true,
+    objectOriented: false,
+  },
+  {
+    name: "Erlang",
+    creator: ["Joe Armstrong", "Robert Virding", "Mike Williams"],
+    releaseYear: 1986,
+    compiled: true,
+    objectOriented: false,
+  },
+  {
+    name: "F#",
+    creator: ["Don Syme", "Microsoft Research"],
+    releaseYear: 2005,
+    compiled: false,
+    objectOriented: false,
+  },
+  {
+    name: "Go",
+    creator: ["Robert Griesemer", "Rob Pike", "Ken Thompson"],
+    releaseYear: 2009,
+    compiled: true,
+    objectOriented: true,
+  },
+  {
+    name: "Ruby",
+    creator: "Yukihiro Matsumoto",
+    releaseYear: 1995,
+    compiled: false,
+    objectOriented: true,
+  },
+  {
+    name: "TypeScript",
+    creator: "Microsoft",
+    releaseYear: 2012,
+    compiled: true,
+    objectOriented: true,
+  },
+];
