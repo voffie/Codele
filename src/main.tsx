d3f406361197a059c83214c474d469856ffe3ae7
VoffieDev <viktorsoderborg@gmail.com>
2023-07-09 13:34:57 +0200

    init

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const container = document.querySelector("#root");
+const root = createRoot(container!);
+
+const Root = () => (
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+root.render(<Root />);
