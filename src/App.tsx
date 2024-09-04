70cc394b564f43cbda72b4a1136091e43ccc854b
voffie <viktorsoderborg@gmail.com>
2024-09-04 22:39:06 +0200

    Clean up logic & layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Terminal } from "./components/Terminal";
+import { Language, getLanguageData, solution } from "./lib/languages";
 import { useEffect, useState } from "react";
-import { Navbar } from "./components/Navbar/Navbar";
-import { FormProvider, useForm } from "react-hook-form";
-import {
-  Language,
-  getGuessData,
-  isLanguageInLanguageList,
-  isWinningLanguage,
-  solution,
-} from "./lib/languages";
 import {
   fetchGameFromLocalStorage,
   writeGameToLocalStorage,
@@ -19,53 +11,30 @@ import { Toaster } from "react-hot-toast";
 import { successToast, errorToast } from "./components/Alerts/Alert";
 
 const App = () => {
-  const { showError: showErrorAlert, showSuccess: showSuccessAlert } =
-    useAlert();
-  const [isGameWon, setIsGameWon] = useState(false);
-  const [isUnlimited, setIsUnlimited] = useState(() => {
-    const loaded = fetchGameFromLocalStorage();
-    return loaded?.isUnlimited ? loaded?.isUnlimited : false;
-  });
-  const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
-  const [isStatsModalOpen, setIsStatsModalOpen] = useState(false);
-  const [isGameLost, setIsGameLost] = useState(false);
-  const [selectedLanguage, setSelectedLanguage] = useState("");
-  const [query, setQuery] = useState("");
   const [guesses, setGuesses] = useState<Language[]>(() => {
     const loaded = fetchGameFromLocalStorage();
     if (loaded?.solution.name !== solution.name) {
       return [];
     }
-    const gameWasWon = loaded?.guesses.find(
-      (item) => item.name === solution.name,
-    )
-      ? true
-      : false;
-    if (gameWasWon) {
-      setIsGameWon(true);
-    }
-    if (!isUnlimited && loaded?.guesses.length === 5 && !gameWasWon) {
-      setIsGameLost(true);
-      showErrorAlert("Game over");
-    }
+
     return loaded?.guesses;
   });
 
-  const [stats, setStats] = useState(() => loadStats());
-
-  useEffect(() => {
-    if (!fetchGameFromLocalStorage()) {
-      setTimeout(() => {
-        setIsInfoModalOpen(true);
-      }, 350);
-    }
-  });
+  const [command, setCommand] = useState(
+    guesses.length > 0 ? ":guess" : ":info",
+  );
+  const [isGameWon, setIsGameWon] = useState(
+    guesses.some((guess) => guess.name === solution.name),
+  );
+  const [isGameLost, setIsGameLost] = useState(
+    guesses.length === 5 &&
+      !guesses.some((guess) => guess.name === solution.name),
+  );
 
   useEffect(() => {
     writeGameToLocalStorage({
       guesses,
       solution,
-      isUnlimited,
     });
   }, [guesses]);
 
@@ -73,31 +42,19 @@ const App = () => {
     if (isGameWon) {
       successToast("You did it!");
     }
-
     if (isGameLost) {
       errorToast("You failed!");
     }
-  }, [isGameWon, isGameLost, showSuccessAlert]);
+  }, [isGameWon, isGameLost]);
 
-  const filteredLanguages =
-    query === ""
-      ? LANGUAGES
-      : LANGUAGES.filter(
-          (lang) =>
-            lang.name.toLowerCase().includes(query.toLowerCase()) ||
-            lang.aliases?.includes(query.toLowerCase()),
-        );
+  function handleGuess(data: string) {
+    setCommand(":guess");
 
-  const methods = useForm();
-  const { handleSubmit } = methods;
-
-  const onSubmit = handleSubmit(() => {
-    if (!isUnlimited && (isGameWon || isGameLost)) {
+    if (isGameWon || isGameLost) {
       return;
     }
 
-    if (guesses.filter((guess) => guess.name === selectedLanguage).length === 1)
-      return showErrorAlert(`Already guessed ${selectedLanguage}`);
+    const language = getLanguageData(data);
 
     if (!language) {
       return errorToast(`Unkown language: ${data}`);
@@ -107,8 +64,7 @@ const App = () => {
       return errorToast(`Already guessed ${language.name}`);
     }
 
-    setGuesses([...guesses, data!]);
-    setSelectedLanguage("");
+    setGuesses([...guesses, language]);
 
     if (solution.name.toLowerCase() === language.name.toLowerCase()) {
       successToast("You guessed the correct language!");
@@ -118,56 +74,34 @@ const App = () => {
     if (guesses.length === 4) {
       errorToast("You lost!");
       setIsGameLost(true);
-      showErrorAlert("Game over");
     }
-  });
-
-  const getClass = (
-    guess: string | string[] | number | boolean,
-    type: string,
-  ) => {
-    switch (type) {
-      case "name":
-        return guess === solution?.name ? "text-green-300" : "text-red-300";
-      case "year":
-        return guess === solution?.releaseYear
-          ? "text-green-300"
-          : "text-red-300";
-      case "compiled":
-        return guess === solution?.compiled ? "text-green-300" : "text-red-300";
-      case "object":
-        if (guess === solution?.objectOriented) return "text-green-300";
-        if (solution?.objectOriented === "true & false") return "";
-        return "text-red-300";
-      case "typed":
-        return guess === solution?.typed ? "text-green-300" : "text-red-300";
-    }
-  };
+  }
 
   return (
+    <main
+      className="flex flex-col overflow-hidden bg-base text-text"
+      style={{ height: window.innerHeight }}
+    >
       <Header />
       <Terminal
         guesses={guesses}
         currentCommand={command}
         isGameWon={isGameWon}
         isGameLost={isGameLost}
-          />
+      />
       <Footer
-            guesses={guesses}
+        guesses={guesses}
         handleGuess={handleGuess}
         setCommand={setCommand}
         isGameWon={isGameWon}
-            isGameLost={isGameLost}
+        isGameLost={isGameLost}
       />
       <Toaster
         position="top-right"
         toastOptions={{ className: "", duration: Infinity }}
         gutter={15}
-          />
-          <AlertContainer />
-        </div>
-      </div>
-    </Div100vh>
+      />
+    </main>
   );
 };
 
