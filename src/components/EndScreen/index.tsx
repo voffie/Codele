c18b5b74f895b1ad1ba7566f5b6eee0a2c6d9cb2
voffie <viktorsoderborg@gmail.com>
2024-09-09 22:32:06 +0200

    Update share logic

--- src/components/EndScreen/index.tsx
+++ src/components/EndScreen/index.tsx
@@ -1,12 +1,21 @@
-import { shareStatus } from "../../lib/share";
+import { generateEmojiGrid, shareStatus } from "../../lib/share";
 import { Language, tomorrow } from "../../lib/languages";
 import Countdown from "react-countdown";
+import { FaTwitter } from "react-icons/fa";
 
 export function EndScreen({ guesses }: { guesses: Language[] }) {
-  const grid = shareStatus(guesses);
+  const uri = shareStatus(guesses);
+  const grid = generateEmojiGrid(guesses);
   return (
     <aside className="mt-20 flex flex-col items-center justify-center gap-4 text-center">
-      <p>Share: {grid}</p>
+      <p className="font-bold">Share</p>
+      <p>
+        Codele: {new Date(Date.now()).toLocaleDateString("en-GB")}{" "}
+        {guesses.length}/5 {grid}
+      </p>
+      <button onClick={() => window.open(uri)}>
+        <FaTwitter />
+      </button>
       <p className="font-bold">
         Next word in <Countdown date={tomorrow} daysInHours />
       </p>
--- src/lib/share.ts
+++ src/lib/share.ts
@@ -3,10 +3,7 @@ import { type Language, solution } from "./languages";
 export function shareStatus(guesses: Language[]) {
   if (!guesses) return;
 
-  return (
-    `Codele ${new Date(Date.now()).toLocaleDateString("en-GB")}\n` +
-    generateEmojiGrid(guesses).join("")
-  );
+  return `https://twitter.com/intent/tweet?text=${generateTwitterShareUri(guesses)}&url=http%3A%2F%2Fcodele-voffiee.vercel.app`;
 }
 
 export function generateEmojiGrid(guesses: Language[]) {
@@ -15,4 +12,12 @@ export function generateEmojiGrid(guesses: Language[]) {
   return guesses.map((guess) => {
     return guess.name === solution.name ? tiles[0] : tiles[1];
   });
+}
+
+export function generateTwitterShareUri(guesses: Language[]) {
+  return encodeURI(
+    `Codele: ${new Date(Date.now()).toLocaleDateString("en-GB")} ${guesses.length}/5\n` +
+      generateEmojiGrid(guesses).join("") +
+      "\n",
+  );
 }
