585367a4f49c276957136dca54114d48b74aef42
voffie <viktorsoderborg@gmail.com>
2024-11-07 21:08:37 +0100

    fix typo & add alert on copy

--- src/components/Share/index.tsx
+++ src/components/Share/index.tsx
@@ -2,6 +2,7 @@ import { generateShareText, generateTwitterShareURI } from "../../lib/share";
 import { type Language, tomorrow } from "../../lib/languages";
 import Countdown from "react-countdown";
 import { FaShareAlt, FaTwitter } from "react-icons/fa";
+import { successToast } from "../../components/Alerts/Alert";
 
 export function Share({
   guesses,
@@ -14,12 +15,17 @@ export function Share({
   const shareText = generateShareText(guesses);
   return (
     <aside className="mt-20 flex flex-col items-center justify-center gap-2 text-center">
-      <p className="font-bold">{`Soution was ${solution.name}`}</p>
+      <p className="font-bold">{`Solution was ${solution.name}`}</p>
       <p className="font-bold">
         Next word in <Countdown date={tomorrow} daysInHours />
       </p>
       <section className="mt-2 flex items-center justify-center gap-4">
-        <button onClick={() => navigator.clipboard.writeText(shareText)}>
+        <button
+          onClick={() => {
+            navigator.clipboard.writeText(shareText);
+            successToast("Successfully copied to clipboard");
+          }}
+        >
           <FaShareAlt />
         </button>
         <button onClick={() => window.open(uri)}>
